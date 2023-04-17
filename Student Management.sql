49a92d878e270cd35847894b6451e1b05d311198
DoChuan13 <chuando.9213@gmail.com>
2023-04-17 11:42:02 +0700

    Update

--- Student Management.sql
+++ Student Management.sql
@@ -1,1 +1,12 @@
-SELECT * FROM Student_Management.Class;
+create database Student_Management;
+use Student_Management;
+create table Class 
+(id int not null auto_increment primary key,
+name varchar(255)
+);
+create table Teacher
+(id int not null auto_increment primary key,
+name varchar(255),
+age int,
+country varchar(255)
+);
